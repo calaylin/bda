390b92c1eaceaf279e022a7ed353fb7ea0b5854a
ubuntu <ubuntu@ip-172-31-11-128.ec2.internal>
2016-02-17 01:18:34 +0000

    changed cleanup

--- src/CleanUpARFFS.java
+++ src/CleanUpARFFS.java
@@ -10,54 +10,68 @@ import weka.core.Instances;
 public class CleanUpARFFS {
 	 public static void main(String[] args) throws Exception, IOException, InterruptedException {
 
-			String output_filename = "/home/ubuntu/Desktop/usenix16/arffs/"
-					+ "github_partial/github_partial/github_concurrent";
+			String output_filename = "/data/usenix16/arffs/L0_600AuthorsConcurrent_part1/"
+				+ "L0_600authors_allfeaturesNoAVGDep_concurrent_part1";
 			
 			
 			int threads=30;
-			for(int no=1; no<=threads+1;no++){
-	    	File arffFile1 = new File(output_filename  +no+".arff");
-	    	File tmpFile = new File(output_filename  +no+"temp.arff");
+			
+			
+			
+			
+			File arffFile1 = new File(output_filename +"1.arff");
+	    	File tmpFile = new File(output_filename  +"Final.arff");
+/*	    	BufferedReader reader = new BufferedReader(new FileReader(tmpFile));
+	    	String line="";
+	    	int lineNo=0;
+	    	while((line = reader.readLine())!= null){
+	    		lineNo++;
+	    	if(lineNo> 1487074 && lineNo< 1487079){
+	    		System.out.println(line);
+	    		Thread.sleep(millis);}}
+	    	reader.close();*/
+	    	
 	    	String line="";
 	    	BufferedReader reader = new BufferedReader(new FileReader(arffFile1));
 	    	BufferedWriter writer  = new BufferedWriter(new FileWriter(tmpFile));
 	    	int counter=0;
+   	
+	   	    	while((line = reader.readLine())!= null){
+	    		
+	    			
+	    		writer.write(line+"\n");}
+	   	    	reader.close();
+			
+			
+			for(int no=2; no<=threads+1;no++){
+	    	 arffFile1 = new File(output_filename  +no+".arff");
+	    	 line="";
+	    	 reader = new BufferedReader(new FileReader(arffFile1));
+	    	 counter=0;
+	    	 String tmp=null;
 	    	while((line = reader.readLine())!= null){
     			//System.out.println(line);
 
 	    	
-	    		if(line.contains("BinaryDeanonymizationAllFeatures")){
+	    		if(line.contains("@data")){
 	    			counter++;
 	    			System.out.println("DETECTED LINE:"+line);
-	    		}}
-	    	
-	    	
-	    	
-	    	
-	    	BufferedReader reader2 = new BufferedReader(new FileReader(arffFile1));
-	    	int counter2=0;
-	    	while((line = reader2.readLine())!= null){
-	    		if(line.contains("BinaryDeanonymizationAllFeatures")){
-	    			counter2++;
 	    		}
-	    		if(counter2==counter){
-	    			if (line.contains("instanceID"))
-	    			{	    		writer.write("@attribute instanceID string"+"\n");}
-	    			if (!(line.contains("instanceID")))
-	    			{
-	    		writer.write(line+"\n");}
-	    			//System.out.println("WRITE LINE:"+line);
-
-	    			}  		
+	    		
+	    		if(counter >0){
+	    			if(!(tmp==null)){
+		    		writer.write(tmp+"\n");}}
+	    			tmp =line;
+	    			
+	    		}
+	    	reader.close();
+	    		
 	    	}
 	    	
 	    	writer.close();
-	    	reader.close();
-	    	tmpFile.renameTo(arffFile1);
 	    	
-			}}
-
-	    	
-	    	
-	}
-	 
+			
+	    	}
+	
+}
+ 
